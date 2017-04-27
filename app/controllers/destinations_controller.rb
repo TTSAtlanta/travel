cd755f6aecb430c2da367b89889456410a1eb7bb
samskinnerphd <samskinnerphd@gmail.com>
2017-04-27 12:06:01 -0400

    scaffold Destination & add images

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,28 +1,26 @@
 class DestinationsController < ApplicationController
   before_action :set_destination, only: [:show, :edit, :update, :destroy]
 
-  # GET /destinations
-  # GET /destinations.json
   def index
-    @destinations = Destination.all
+    if params[:country] == nil
+      @destinations = Destination.all
+    else
+      @destinations = Destination.where(country: params[:country].titleize)
+    end
+
+    @my_countries = Destination.select(:country).order(:country).distinct
   end
 
-  # GET /destinations/1
-  # GET /destinations/1.json
   def show
   end
 
-  # GET /destinations/new
   def new
     @destination = Destination.new
   end
 
-  # GET /destinations/1/edit
   def edit
   end
 
-  # POST /destinations
-  # POST /destinations.json
   def create
     @destination = Destination.new(destination_params)
 
@@ -37,8 +35,6 @@ class DestinationsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /destinations/1
-  # PATCH/PUT /destinations/1.json
   def update
     respond_to do |format|
       if @destination.update(destination_params)
@@ -51,12 +47,10 @@ class DestinationsController < ApplicationController
     end
   end
 
-  # DELETE /destinations/1
-  # DELETE /destinations/1.json
   def destroy
     @destination.destroy
     respond_to do |format|
-      format.html { redirect_to destinations_url, notice: 'Destination was successfully destroyed.' }
+      format.html { redirect_to destinations_url, notice: 'Poof!  Gone just like that!' }
       format.json { head :no_content }
     end
   end
@@ -69,6 +63,6 @@ class DestinationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def destination_params
-      params.require(:destination).permit(:city, :country, :description)
+      params.require(:destination).permit(:city, :country, :description, :population, :avatar)
     end
 end
