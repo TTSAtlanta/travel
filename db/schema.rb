aeeafb3a4c660a0bf8b0bb3e997a4351421e511b
samskinnerphd <samskinnerphd@gmail.com>
2017-04-27 12:06:35 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,14 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111123839) do
+ActiveRecord::Schema.define(version: 20170426204432) do
 
-  create_table "destinations", force: true do |t|
+  create_table "destinations", force: :cascade do |t|
     t.string   "city"
     t.string   "country"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "population"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
